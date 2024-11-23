0cec8c3b7b853c51a4e9790d0b8f91d1cc68a16c
Marlo3110 <marlo.mannheim@outlook.de>
2024-11-23 22:30:38 +0100

    Creating Head point.

---
+++ PythonVisualization/BaseParts/Window.py
@@ -0,0 +1,9 @@
+class Window:
+    def __init__(self, WindowScaleType = "AbsoluteScale", WindowSizeX = 500, WindowSizeY = 500, WindowAspectRatioX = 0.50, WindowAspectRatioY = 0.50):
+        self.WindowScaleType = WindowScaleType
+
+        self.WindowSizeX = WindowSizeX
+        self.WindowSizeY = WindowSizeY
+
+        self.AspectRatioX = WindowAspectRatioX
+        self.AspectRatioY = WindowAspectRatioY
